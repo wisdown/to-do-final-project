5a9ac16ca4f53585e01087ceca70f09eefb83187
Elí Gómez <blessedinyou@gmail.com>
2022-10-09 23:03:03 -0600

    instalacion de expres y cracion de la api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
+import Express from "express";
 import { initDb } from "./db/index.js";
 
-initDb().then(() => {
-  console.log("DB Created successfully");
+const Api = Express();
+
+// middleware para convertir a formato Jason.
+Api.use(Express.json());
+// middleware para proteger la  url
+Api.use(Express.urlencoded({ extended: false }));
+
+Api.listen(3000, () => {
+  console.log("API IS RUNNING :)\n");
+  initDb().then(() => {
+    console.log("DB Created successfully:)");
+  });
 });
